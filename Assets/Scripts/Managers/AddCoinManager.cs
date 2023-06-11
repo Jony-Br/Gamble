16ac6cde3c00883680e1b8463af946b29d04e749
JonyBr <Jelatinbumbir252@gmail.com>
2023-06-11 16:58:00 +0300

    little change

--- Assets/Scripts/Managers/AddCoinManager.cs
+++ Assets/Scripts/Managers/AddCoinManager.cs
@@ -12,21 +12,30 @@ public class AddCoinManager : MonoBehaviour
     [SerializeField] private UnityEngine.UI.Button _plusButton;
     [SerializeField] private UnityEngine.UI.Button _minusButton;
     [SerializeField] private UnityEngine.UI.Button _addButton;
+    [SerializeField] private UnityEngine.UI.Button _exitButton;
     [SerializeField] private TMP_InputField _inputField;
     [SerializeField] private TextMeshProUGUI _coins;
-    private int _coinsNumber;
+    private int _coinsNumber = 0;
 
+    private void Awake()
+    {
+        _coinsNumber = 0;
+        _coins.text = _coinsNumber.ToString();
+        _inputField.text = "0";
+    }
     void Start()
     {
         _addButton.onClick.AddListener(AddCoinHandler);
         _plusButton.onClick.AddListener(OnPlusButtonClick);
         _minusButton.onClick.AddListener(OnMinusButtonClick);
+        _exitButton.onClick.AddListener(OnExitButtonClick);
         
+
     }
 
     void Update()
     {
-
+        _coins.text = _coinsNumber.ToString();
     }
 
     void OnPlusButtonClick()
@@ -47,10 +56,17 @@ public class AddCoinManager : MonoBehaviour
 
     void AddCoinHandler()
     {
+
         _coinsNumber  = Convert.ToInt32(_inputField.text);
+
         //_inputField.text = _coinsNumber.ToString();
 
         MoneyManager.Instance.money += _coinsNumber;
+        gameObject.SetActive(false);
+
+    }
+    void OnExitButtonClick()
+    {
         gameObject.SetActive(false);
     }
 }
